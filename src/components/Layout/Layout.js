eadd3dfc922773fee93a068cc83e62fafe18d273
redwoodcutter <oguzkumcular@gmail.com>
2020-10-08 23:49:30 +0300

    Layouts Create

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import classes from './layout.css';
 const layout= (props) => (
     <Aux>
         <div>
             toolbar, sidedivider, backdrop
         </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
