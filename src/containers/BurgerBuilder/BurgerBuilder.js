23d4ddee61b4815ad5a04c10f9092d88e2182d38
redwoodcutter <oguzkumcular@gmail.com>
2020-10-11 15:05:20 +0300

    Burger ingredients Dynamicaly

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,25 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    //constructor(props){
+    //    super(props);
+    //        this.state={...}
+    //    }
+    // }
+    //object not a array -- >
+    state={
+        ingredients: {
+            sala: 1,
+            bacon: 1,
+            cheese: 2,
+            meat:2,
+        }
+
+    }
     render(){
         return(
            <Aux>
-               <Burger />
+               <Burger ingredients={this.state.ingredients} />
                <div>Build Controls</div>
            </Aux>
         );
