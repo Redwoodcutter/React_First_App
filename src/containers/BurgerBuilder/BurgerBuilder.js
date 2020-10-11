2ddf6501335eb4c31cac1d63454b5020ac5decd8
redwoodcutter <oguzkumcular@gmail.com>
2020-10-12 00:24:31 +0300

    Build Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     //constructor(props){
@@ -11,10 +12,10 @@ class BurgerBuilder extends Component {
     //object not a array -- >
     state={
         ingredients: {
-            sala: 0,
-            bacon: 0,
-            cheese: 0,
-            meat:0,
+            sala: 1,
+            bacon: 1,
+            cheese: 2,
+            meat:2,
         }
 
     }
@@ -22,7 +23,7 @@ class BurgerBuilder extends Component {
         return(
            <Aux>
                <Burger ingredients={this.state.ingredients} />
-               <div>Build Controls</div>
+               <BuildControls />
            </Aux>
         );
     }
